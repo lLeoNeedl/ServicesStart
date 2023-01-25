a2d3830137e1b14d0b9e0264673ffb19566bb727
Leonid Lavrov <274852@gmail.com>
2023-01-25 15:18:08 +0200

    Working with JobService

--- app/src/main/java/com/example/servicesstart/MainActivity.kt
+++ app/src/main/java/com/example/servicesstart/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.servicesstart
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
+import android.content.ComponentName
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import com.example.servicesstart.MyJobService.Companion.JOB_ID
 import com.example.servicesstart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +20,19 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private var page = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.simpleService.setOnClickListener {
             stopService(MyForegroundService.newIntent(this))
             startService(MyService.newIntent(this, 25))
+//            ContextCompat.startForegroundService(
+//                this,
+//                MyService.newIntent(this, 25)
+//            )
         }
-
         binding.foregroundService.setOnClickListener {
             ContextCompat.startForegroundService(
                 this,
@@ -34,6 +44,20 @@ class MainActivity : AppCompatActivity() {
                 this,
                 MyIntentService.newIntent(this)
             )
+        }
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(JOB_ID, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .build()
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
         }
     }
 
