b0d90a4b2680b3f37ab95c82ab535d80ab20d3e5
Leonid Lavrov <274852@gmail.com>
2023-01-25 15:48:17 +0200

    JobScheduler and IntentService combination

---
+++ app/src/main/java/com/example/servicesstart/MyIntentServiceNotForeground.kt
@@ -0,0 +1,44 @@
+package com.example.servicesstart
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class MyIntentServiceNotForeground : IntentService(NAME) {
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+        setIntentRedelivery(true)
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        log("onHandleIntent")
+        val page = intent?.getIntExtra(PAGE, 0) ?: 0
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i, $page")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyForegroundService: $message")
+    }
+
+    companion object {
+
+        private const val NAME = "MyIntentService"
+        private const val PAGE = "page"
+
+        fun newIntent(context: Context, page: Int) =
+            Intent(context, MyIntentServiceNotForeground::class.java).apply {
+                putExtra(PAGE, page)
+            }
+    }
+}
